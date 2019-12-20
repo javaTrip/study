7513cd7fbdf910dffcbd1748b47f70990579e2d2
haochuanji <1253653941@qq.com>
2019-12-20 14:24:10 +0800

    no message

--- src/main/java/com/fh/controller/student.java
+++ src/main/java/com/fh/controller/student.java
@@ -1,4 +1,20 @@
 package com.fh.controller;
 
 public class student {
+
+    public   String   bbb(){
+
+
+        return  "ni hao " ;
+    }
+
+
+
+
+    public  String   bb(){
+
+        return  "henhao" ;
+    }
+
+
 }
